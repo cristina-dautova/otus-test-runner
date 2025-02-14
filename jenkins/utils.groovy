223414b721ebce650ccaeb34a0686f0198140455
cristina-dautova <70657896+cristina-dautova@users.noreply.github.com>
2025-02-14 13:37:36 +0300

    Update utils.groovy

--- jenkins/utils.groovy
+++ jenkins/utils.groovy
@@ -2,9 +2,9 @@ def prepare_yaml_config() {
 
     def config = readYaml text: $CONFIG
 
-    config.each(k,v -> {
-        env.setProperty(k,v)
-    })
+    config.each { k, v ->  
+        env[k] = v.toString()
+    }
 }
 
 return this
