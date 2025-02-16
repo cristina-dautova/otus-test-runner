6aadefc23c5d4fa2d31f03acd407f7b72ea4147d
cristina-dautova <70657896+cristina-dautova@users.noreply.github.com>
2025-02-16 14:48:47 +0300

    Update utils.groovy

--- jenkins/utils.groovy
+++ jenkins/utils.groovy
@@ -1,5 +1,9 @@
 def prepare_yaml_config() {
 
+    if(!env.CONFIG){
+        error "CONFIG environment variable is not set."
+    }
+    
     def config = readYaml text: env.CONFIG
 
     config.each { k, v ->  
